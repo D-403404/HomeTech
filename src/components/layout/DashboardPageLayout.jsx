93056d2723853e9f9b840d32bf2c3faa03b52e5f
UserCoder <113160520+D-403404@users.noreply.github.com>
2024-04-28 19:22:22 +0700

    1.3.1: Dashboard small fix

--- src/components/layout/DashboardPageLayout.jsx
+++ src/components/layout/DashboardPageLayout.jsx
@@ -3,9 +3,21 @@ import ReturnButton from "/src/components/general/ReturnButton";
 import Arc_temperature from "/src/assets/images/Arc_temperature.png";
 import Arc_humidity from "/src/assets/images/Arc_humidity.png";
 
-export default function DashboardPageLayout({ title, value, unit, range, safeRange, warningMsgLst }) {
+export default function DashboardPageLayout({
+  title,
+  value,
+  unit,
+  range,
+  safeRange,
+  warningMsgLst,
+}) {
   const Arc = title === "Temperature" ? Arc_temperature : Arc_humidity;
-  const msg = value < safeRange[0] ? warningMsgLst[0] : value > safeRange[1] ? warningMsgLst[1] : "";
+  const msg =
+    value < safeRange[0]
+      ? warningMsgLst[0]
+      : value > safeRange[1]
+      ? warningMsgLst[1]
+      : "";
 
   return (
     <div className="relative min-h-[100vh] min-w-[1001px] bg-background">
@@ -15,15 +27,15 @@ export default function DashboardPageLayout({ title, value, unit, range, safeRan
           {title}
         </div>
         <Chart Arc={Arc} range={range} value={value} unit={unit} />
-        {(value < safeRange[0] || value > safeRange[1]) &&
-        <div className="pl-[50px] textwarning">
-          <span className="font-[Alata]">
-            <span className="underline">Warning</span>
-            <span>:</span>
-          </span>
-          <span className="font-[Actor]"> {msg}</span>
-        </div>
-}
+        {value !== null && (value < safeRange[0] || value > safeRange[1]) && (
+          <div className="pl-[50px] textwarning">
+            <span className="font-[Alata]">
+              <span className="underline">Warning</span>
+              <span>:</span>
+            </span>
+            <span className="font-[Actor]"> {msg}</span>
+          </div>
+        )}
       </div>
     </div>
   );
@@ -33,6 +45,7 @@ function Chart({ Arc, range, value, unit }) {
   const [angle, setAngle] = useState(45);
 
   function angleFromValue() {
+    if (value === null) return;
     if (value < range[0]) return 0;
     if (value > range[1]) return 1;
     return (value - range[0]) / (range[1] - range[0]);
@@ -41,6 +54,7 @@ function Chart({ Arc, range, value, unit }) {
   // Counterclockwise: -45deg to 45deg
   useEffect(() => {
     const perc = angleFromValue();
+    if (perc === null) return;
     console.log(perc);
     if (perc > 0.5) setAngle(-45 - (1 - perc) * (360 - 90));
     else setAngle(45 + perc * (360 - 90));
@@ -56,18 +70,23 @@ function Chart({ Arc, range, value, unit }) {
     <div className="flex justify-center items-center">
       <div className="relative h-[460px] w-[506px] flex justify-center items-center">
         <img className="absolute top-0 w-[506px] z-0" src={Arc} />
-        <div className="absolute bottom-0 left-[-15px] font-[Adamina] text-[40px]">{range[0]}</div>
-        <div className="absolute bottom-0 right-[-35px] font-[Adamina] text-[40px]">{range[1]}</div>
+        <div className="absolute bottom-0 left-[-15px] font-[Adamina] text-[40px]">
+          {range[0]}
+        </div>
+        <div className="absolute bottom-0 right-[-35px] font-[Adamina] text-[40px]">
+          {range[1]}
+        </div>
         <div className="font-[Adamina] text-[100px] z-50">
-          {value.toFixed(1)}
-          {unit}
+          {value !== null ? value.toFixed(1).toString() + unit : "N/A"}
         </div>
-        <div
-          className="absolute left-[calc(50%-42.5px)] top-[-22.80395px] w-[85px] h-[calc(506px+22.80395px*2)] flex flex-col justify-end"
-          style={{ rotate: `${angle}deg` }}
-        >
-          <div className="dot"></div>
-        </div>
+        {value !== null && (
+          <div
+            className="absolute left-[calc(50%-42.5px)] top-[-22.80395px] w-[85px] h-[calc(506px+22.80395px*2)] flex flex-col justify-end"
+            style={{ rotate: `${angle}deg` }}
+          >
+            <div className="dot"></div>
+          </div>
+        )}
       </div>
     </div>
   );
